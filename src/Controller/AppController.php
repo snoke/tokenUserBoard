b1487e2d7a4d144f07aea8e0661b5fd06387db7c
unknown <stowwel@gmail.com>
2021-06-21 05:50:44 +0200

    tidy up

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -7,15 +7,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface as Encoder;
 
-#[Route('/app')]
 class AppController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
+    #[Route('/app', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('app/index.html.twig', [
         ]);
     }
 
+    /**
+     * @Route("api/me", methods={"GET"})
+     * 
+     * this method extracts user credentials stored as payload within the token
+     */
+    public function getCredentials(Encoder $jwtEncoder,Request $request): Response
+    {
+        $data = $jwtEncoder->decode(substr($request->headers->get('Authorization'),7));
+        return $this->json($data);
+    }
 }
