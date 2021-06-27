636257a76f60d23b74d0e04de2843f73df3d19a6
unknown <stowwel@gmail.com>
2021-06-28 00:24:13 +0200

    adding auth landing page

--- assets/components/Auth/Auth.vue
+++ assets/components/Auth/Auth.vue
@@ -1,7 +1,40 @@
 <template>
-<div>
-    <router-view />
-</div>
+  <div class="w-100 pt" style="float:left;">
+      <ul class="list-group w-100">
+          <li class="list-group-item w-100" v-if="$root.user.roles.includes('ROLE_GUEST')">
+              <router-link :to="{ name: 'Login'}" >
+                    <div class="row">
+                      <div class="col-lg d-flex justify-content-start ">
+                          Login
+                      </div>
+                    </div>
+              </router-link>
+          </li>
+          <li class="list-group-item w-100" v-if="$root.user.roles.includes('ROLE_GUEST')">
+              <router-link :to="{ name: 'Register'}" >
+                    <div class="row">
+                      <div class="col-lg d-flex justify-content-start ">
+                          Register
+                      </div>
+                    </div>
+              </router-link>
+          </li>
+          <li class="list-group-item w-100" v-if="!$root.user.roles.includes('ROLE_GUEST')">
+              <router-link :to="{ name: 'Logout'}" >
+                    <div class="row">
+                      <div class="col-lg d-flex justify-content-start ">
+                          Logout
+                      </div>
+                    </div>
+              </router-link>
+          </li>
+    </ul>
+        <div  v-if="this.loading == 1" class=" d-flex justify-content-center w-100 p-3">
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only" ></span>
+            </div>
+        </div>
+  </div> 
 </template>
 
 
@@ -16,6 +49,7 @@ export default {
     },
      data() {
       return {
+          loading:0
       }
     },
     mounted () {
--- assets/components/User/User.vue
+++ assets/components/User/User.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="w-100" style="float:left;">
-    <router-view />
+  <div class="w-100 pt" style="float:left;">
+      <ul class="list-group w-100">
+          <li class="list-group-item w-100" >
+              <router-link :to="{ name: 'UserProfile', params: { username: $root.user.username}}" >
+                    <div class="row">
+                      <div class="col-lg d-flex justify-content-start ">
+                          Einstellungen
+                      </div>
+                    </div>
+              </router-link>
+          </li>
+    </ul>
+        <div  v-if="this.loading == 1" class=" d-flex justify-content-center w-100 p-3">
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only" ></span>
+            </div>
+        </div>
   </div> 
 </template>
 
